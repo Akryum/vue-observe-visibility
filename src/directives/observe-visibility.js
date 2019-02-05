f80a59ce10aa829bfef0f9af7100cdbe5892c291
Rob Taylor <robert.taylor2@vivint.com>
2019-02-05 13:11:03 -0700

    fixed observer to reference the last state in entries

--- src/directives/observe-visibility.js
+++ src/directives/observe-visibility.js
@@ -30,7 +30,7 @@ class VisibilityState {
 		this.oldResult = undefined
 
 		this.observer = new IntersectionObserver(entries => {
-			var entry = entries[0]
+			var entry = entries[entries.length - 1]
 			if (this.callback) {
 				// Use isIntersecting if possible because browsers can report isIntersecting as true, but intersectionRatio as 0, when something very slowly enters the viewport.
 				const result = entry.isIntersecting && entry.intersectionRatio >= this.threshold
